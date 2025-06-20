a36ab5ade847b96a18d52475b2b51fe69b87113a
XenovoBots <XenovoBots@Gmail.Com>
2025-06-20 19:50:12 +0530

    Xenovo Bots

--- Abhi/Utils/YouTube/Search.py
+++ Abhi/Utils/YouTube/Search.py
@@ -1,1 +1,25 @@
+import asyncio
+from YouTubeMusic.Search import Search
 
+
+async def SearchYt(query: str):
+    results = await Search(query, limit=1)
+
+    if not results or not results.get("main_results"):
+        raise Exception("No results found.")
+
+    item = results["main_results"][0] 
+
+    search_data = [{
+        "title": item.get("title"),
+        "artist": item.get("artist_name"),
+        "channel": item.get("channel_name"),
+        "duration": item.get("duration"),
+        "views": item.get("views"),
+        "thumbnail": item.get("thumbnail"),
+        "url": item.get("url")
+    }]
+
+    song_link = item["url"] 
+
+    return search_data, song_link

f58a7124a770dcd8dc9827e422dc87d46da622a3
XenovoBots <XenovoBots@Gmail.Com>
2025-06-21 01:13:29 +0530

    Xenovo Bots

--- Abhi/Database/__init__.py
+++ Abhi/Database/__init__.py
@@ -1,11 +1,1 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from datetime import datetime, timezone
-from Config import Config
 from .YouTube import ensure_indexes, get_cached_search, add_cached_search
-
-
-client = AsyncIOMotorClient(Config.MONGO_URI)
-db = client["MusicBot"]
-
-search_collection = db["search_cache"]
-stream_collection = db["stream_cache"]

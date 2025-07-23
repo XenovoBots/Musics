f261411913bbff8dc19e1223422e8eb376ad53d5
XenovoBots <XenovoBots@Gmail.Com>
2025-07-24 01:25:15 +0530

    Update Search.py

--- Abhi/Utils/YouTube/Search.py
+++ Abhi/Utils/YouTube/Search.py
@@ -7,14 +7,14 @@ async def SearchYt(query: str):
     await ensure_indexes()
 
     normalized_query = query.lower().strip()
-    start = time.time()  # Start timing
+    #start = time.time()  # Start timing
 
     cached = await get_cached_search(normalized_query)
     if cached:
-        print("✅ Using cached search result")
+       # print("✅ Using cached search result")
         item = cached["result"]
     else:
-        print("🔍 Performing fresh search...")
+      #  print("🔍 Performing fresh search...")
         results = await Search(query, limit=1)
         if not results or not results.get("main_results"):
             raise Exception("❌ No results found.")
@@ -22,8 +22,8 @@ async def SearchYt(query: str):
         item = results["main_results"][0]
         await add_cached_search(normalized_query, item)
 
-    end = time.time()  # End timing
-    print(f"⏱️ Search Time Taken: {end - start:.2f} seconds")
+  $  end = time.time()  # End timing
+   # print(f"⏱️ Search Time Taken: {end - start:.2f} seconds")
 
     # Format output
     search_data = [{
